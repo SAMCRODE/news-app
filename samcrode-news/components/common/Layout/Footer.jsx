0530af3907edd6c276d2dbd700b5cd8c517bd248
rafaelcsva <rafaelgyncastro63@gmail.com>
2021-10-17 17:33:27 -0300

    Update next; add github link on footer; put shadow on footer and navbar

--- samcrode-news/components/common/Layout/Footer.jsx
+++ samcrode-news/components/common/Layout/Footer.jsx
@@ -1,9 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import {
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons';
 import styles from './layout.module.scss';
 
 const Footer = () => (
   <div className={styles.fcontainer}>
-    <p>SAMCRODE nem todos os direitos reservados</p>
+    SAMCRODE nem todos os direitos reservados
+    <a href="https://github.com/samcrode" target="_blank" rel="noreferrer">
+      <FontAwesomeIcon icon={faGithub} className={styles.footlink} style={{ fontSize: '2px' }} />
+    </a>
   </div>
 );
 
